d322aa06e715692caee776edeb245cef8df327f4
Curro Raya <Curroraya@hotmail.com>
2021-05-09 21:22:43 +0200

    Login y registro terminado sin backend

---
+++ lib/pages/login_page.dart
@@ -0,0 +1,82 @@
+import 'package:chatapp/widgets/boton_azul.dart';
+import 'package:flutter/material.dart';
+
+import 'package:chatapp/widgets/labels.dart';
+import 'package:chatapp/widgets/logoImagen.dart';
+import 'package:chatapp/widgets/custom_input.dart';
+
+
+class LoginPage extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color(0xffF2F2F2),
+      body: SafeArea(
+        child: SingleChildScrollView(
+          physics: BouncingScrollPhysics(),
+          child: Container(
+            height: MediaQuery.of(context).size.height * 0.92,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                LogoImagen(titulo: 'Messenger'),
+                _Form(),
+                Labels(
+                  titulo: '¿No tienes cuenta?', 
+                  subtitulo: '¡Crea una ahora!',
+                  ruta: 'register',
+                ),
+                Text(
+                  'Terminos y Condiciones de uso', 
+                  style: TextStyle(
+                    fontWeight: FontWeight.w200
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      )
+   );
+  }
+}
+
+class _Form extends StatefulWidget {
+  @override
+  __FormState createState() => __FormState();
+}
+
+class __FormState extends State<_Form> {
+
+  final emailCtrl = TextEditingController();
+  final passCtrl = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.only(top: 40),
+      padding: EdgeInsets.symmetric(horizontal: 50),
+      child: Column(
+        children: [
+          CustomInput(
+            icon: Icons.mail_outline,
+            placeHolder: 'Email', 
+            keyboardType: TextInputType.emailAddress,
+            textController: emailCtrl,
+          ),
+          CustomInput(
+             icon: Icons.lock_outline,
+             placeHolder: 'Contraseña',
+             textController: passCtrl,
+             isPassword: true,
+          ),
+          BotonAzul(
+            text: 'Login',
+            onPressed: (){},
+          )
+        ],
+      ),
+    );
+  }
+}
